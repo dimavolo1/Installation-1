d61baf16b7788b03f7c7c39608ecb0a58172d78c
dimavolo1 <125883080+dimavolo1@users.noreply.github.com>
2023-07-03 12:36:44 +0500

    1

---
+++ src/GameProgress.java
@@ -0,0 +1,82 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
+public class GameProgress implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private final int health;
+    private final int weapons;
+    private final int lvl;
+    private final double distance;
+
+    public GameProgress(int health, int weapons, int lvl, double distance) {
+        this.health = health;
+        this.weapons = weapons;
+        this.lvl = lvl;
+        this.distance = distance;
+    }
+
+    public void saveGame(String input, GameProgress gameProgress) {
+        try (FileOutputStream fos = new FileOutputStream(input)) {
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+            oos.writeObject(gameProgress);
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public void zipFiles(String output, String input) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(output)); FileInputStream fis = new FileInputStream(input)) {
+            String name = input.substring(input.lastIndexOf("/") + 1);
+            ZipEntry entry = new ZipEntry("packed_" + name);
+            zout.putNextEntry(entry);
+            byte[] buffer = new byte[fis.available()];
+            fis.read(buffer);
+            zout.write(buffer);
+            zout.closeEntry();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public void openZip(String input, String output) {
+        try (ZipInputStream zis = new ZipInputStream(new FileInputStream(input))) {
+            ZipEntry entry;
+            String name;
+            while ((entry = zis.getNextEntry()) != null) {
+                name = entry.getName();
+                name = name.substring(name.lastIndexOf("_") + 1);
+                FileOutputStream fos = new FileOutputStream(output + name);
+                for (int i = zis.read(); i != -1; i = zis.read()) {
+                    fos.write(i);
+                }
+                fos.flush();
+                zis.closeEntry();
+                fos.close();
+            }
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public GameProgress openProgress(String input) {
+        GameProgress gameProgress = null;
+        try (FileInputStream fis = new FileInputStream(input); ObjectInputStream ois = new ObjectInputStream(fis)) {
+            gameProgress = (GameProgress) ois.readObject();
+        } catch (Exception exception) {
+            System.out.println(exception.getMessage());
+        }
+        return gameProgress;
+    }
+
+    @Override
+    public String toString() {
+        return "GameProgress{" + "health=" + health + ", weapons=" + weapons + ", lvl=" + lvl + ", distance=" + distance + '}';
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,128 @@
+import java.io.*;
+
+public class Main {
+    private final static String ROOT = "/Users/ostrovsky/Games/";
+
+    public static void main(String[] args) {
+        StringBuilder log = new StringBuilder();
+        int idx = 0;
+        File[] dirs = {
+                new File(ROOT + "src"),
+                new File(ROOT + "res"),
+                new File(ROOT + "savegames"),
+                new File(ROOT + "temp"),
+        };
+        for (File dir: dirs) {
+            if (dir.mkdir()) {
+                idx++;
+                log.append(idx).append(". Каталог ").append(dir.getName()).append(" создан").append('\n');
+            }
+        }
+
+        File[] srcDirs = {
+                new File(ROOT + "src/main"),
+                new File(ROOT + "src/test"),
+        };
+        if (dirs[0].exists()) {
+            for (File dir: srcDirs) {
+                if (dir.mkdir()) {
+                    idx++;
+                    log.append(idx).append(". Каталог ").append(dir.getName()).append(" создан").append('\n');
+                }
+            }
+        }
+
+
+        File[] mainFiles = {
+                new File(ROOT + "src/main/Main.java"),
+                new File(ROOT + "src/main/Utils.java"),
+        };
+
+        if (srcDirs[0].exists()) {
+            for (File file: mainFiles) {
+                try {
+                    if (file.createNewFile()) {
+                        idx++;
+                        log.append(idx).append(". Файл ").append(file.getName()).append(" создан").append('\n');
+                    };
+                } catch (IOException io) {
+                    System.out.println(io.getMessage());
+                }
+            }
+        }
+
+        File[] resDirs = {
+                new File(ROOT + "res/drawables"),
+                new File(ROOT + "res/vectors"),
+                new File(ROOT + "res/icons"),
+        };
+
+        if (dirs[1].exists()) {
+            for (File dir: resDirs) {
+                if (dir.mkdir()) {
+                    idx++;
+                    log.append(idx).append(". Каталог ").append(dir.getName()).append(" создан").append('\n');
+                }
+            }
+        }
+
+        File tempFile = new File(ROOT + "temp/temp.txt");
+        try {
+            if (tempFile.createNewFile()) {
+                idx++;
+                log.append(idx).append(". Файл ").append(tempFile.getName()).append(" создан").append('\n');
+            }
+        } catch (IOException io) {
+            System.out.println(io.getMessage());
+        }
+
+        String completeLog = log.toString();
+
+        if (tempFile.exists()) {
+            try (FileWriter fw = new FileWriter(tempFile)) {
+                fw.write(completeLog);
+                fw.flush();
+            } catch (IOException io) {
+                System.out.println(io.getMessage());
+            }
+        }
+
+        /*
+            task 2
+         */
+
+        GameProgress gameProgress1 = new GameProgress(100, 2, 2, 1.2);
+        GameProgress gameProgress2 = new GameProgress(97, 4, 3, 2.1);
+        GameProgress gameProgress3 = new GameProgress(98, 4, 3, 2.2);
+
+        gameProgress1.saveGame(ROOT + "savegames/save1.dat", gameProgress1);
+        gameProgress1.zipFiles(ROOT + "savegames/zip1.zip", ROOT + "savegames/save1.dat");
+
+        gameProgress2.saveGame(ROOT + "savegames/save2.dat", gameProgress2);
+        gameProgress1.zipFiles(ROOT + "savegames/zip2.zip", ROOT + "savegames/save2.dat");
+
+        gameProgress3.saveGame(ROOT + "savegames/save3.dat", gameProgress3);
+        gameProgress1.zipFiles(ROOT + "savegames/zip3.zip", ROOT + "savegames/save3.dat");
+
+        // remove all non-zipped files
+        for (File save: dirs[2].listFiles()) {
+            if (save.getName().contains(".dat")) {
+                if (save.delete()) {
+                    continue;
+                };
+            }
+        }
+
+        /*
+            task 3
+         */
+
+        gameProgress1.openZip(ROOT + "savegames/zip1.zip", ROOT + "savegames/");
+        gameProgress2.openZip(ROOT + "savegames/zip2.zip", ROOT + "savegames/");
+        gameProgress3.openZip(ROOT + "savegames/zip3.zip", ROOT + "savegames/");
+
+        System.out.println(gameProgress1.openProgress(ROOT + "savegames/save1.dat"));
+        System.out.println(gameProgress2.openProgress(ROOT + "savegames/save2.dat"));
+        System.out.println(gameProgress3.openProgress(ROOT + "savegames/save3.dat"));
+    }
+}
